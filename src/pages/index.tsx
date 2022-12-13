f68e595562e86499bdd0547663b887464537d5a9
Felipe Lopes <felipe.presidencia@gmail.com>
2022-12-12 23:37:03 -0300

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from '../../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div>
+        <img src="/logo.svg" alt="" width={190} height={60} />
+        <p>UTILIZE O LEITOR DE CÓDIGO DE BARRAS
+          PARA IDENTIFICAÇÃO DO PRODUTO
+          OU SELECIONE ABAIXO:</p>
+      </div>
+    </div>
+  )
+}
