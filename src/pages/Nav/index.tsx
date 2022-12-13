98b8170905ce4264fb9d9b75dfc75c5abd69aa81
Felipe Lopes da Silva <flsilva@anp.gov.br>
2022-12-13 18:02:13 -0300

    C2 - Styles-pages

--- src/pages/Nav/index.tsx
+++ src/pages/Nav/index.tsx
@@ -1,45 +1,21 @@
 import styles from './styles.module.scss';
+import { Button } from 'react-bootstrap';
+
 
 export default function Nav() {
   return (
-    <nav className={styles.table}>
-      <ul>
-        <li>
-          <a href="#">
-            PROMOÇÕES
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            IOGURTE
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            SNACKS
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            FRIOS
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            SANDUÍCHES
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            BEBIDAS REFRIGERADAS
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            BEBIDAS QUENTES
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <table className={styles.table}>
+      <thead>
+        <tr className={styles.tr1}>
+          <th>PROMOÇÕES</th>
+          <th>IOGURTE</th>
+          <th>SNACKS</th>
+          <th>FRIOS</th>
+          <th>SANDUÍCHE</th>
+          <th>BEBIDAS REFRIGERADAS</th>
+          <th>BEBIDAS QUENTES</th>
+        </tr>
+      </thead>
+    </table>
   )
 }
